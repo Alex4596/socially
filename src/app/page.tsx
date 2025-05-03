4f2ea34b79146b1189d23895ab224f91d2c5879a
Alex4596 <losgary058@gmail.com>
2025-05-03 14:29:42 +0200

    Following list(Who To Follow) and Follows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import {currentUser} from "@clerk/nextjs/server";
 import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
 
 export default async function Home() {
 
@@ -11,7 +12,9 @@ export default async function Home() {
                  {user ? <CreatePost /> : null}
              </div>
 
-             <div className="hidden lg:block lg:col-span-4 sticky top-10">WhoToFollow</div>
+             <div className="hidden lg:block lg:col-span-4 sticky top-10">
+                   <WhoToFollow />
+             </div>
          </div>
     );
 }
